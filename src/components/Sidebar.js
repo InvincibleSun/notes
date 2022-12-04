05f132c081fef0637a8775740f5fa9939ee277e1
InvincibleSun <103495967+InvincibleSun@users.noreply.github.com>
2022-12-04 20:10:37 +0400

    added trash bin icon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,6 +11,9 @@ export default function Sidebar(props) {
           onClick={() => props.setCurrentNoteId(note.id)}
         >
           <h4 className="text-snippet">{title}</h4>
+          <button className="delete-btn">
+            <i className="gg-trash trash-icon"></i>
+          </button>
         </div>
       </div>
     );
